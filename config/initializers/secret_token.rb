7de57f725d14ae2948a39f01b4b5c5fbe802b7f8
Pavel Efimov <PEfimov@MorganStout.com>
2012-03-11 11:47:18 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zavod::Application.config.secret_token = '242fca68e89e1aa17391e44c1680ee85c4ed279ca25f2494b1d52158cc56c2abb1816dfaa481fc2e403d8f5731e1734f3cc23765eb8bd7376d801913d2cff8af'
