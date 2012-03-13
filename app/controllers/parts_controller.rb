48926c7c490422b331131c14f6713804aff469f4
Pavel Yankie Yefimov <yankie@inbox.ru>
2012-03-13 10:34:02 +0400

    Moved Blueprint to vendor dir since it's rails' 3.2 way Fixed styles Fixed some issues

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,83 +1,97 @@
 class PartsController < ApplicationController
-  # GET /parts
-  # GET /parts.json
-  def index
-    @parts = Part.all
+	# GET /parts
+	# GET /parts.json
+	def index
+		@parts = Part.all
+		@product = Product.find(params[:product_id])
+		@supplier = Supplier.find(params[:supplier_id])
+		                                 
+		respond_to do |format|
+		format.html # index.html.erb
+		format.json { render json: @parts }
+		end
+	end
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @parts }
-    end
-  end
+	# GET /parts/1
+	# GET /parts/1.json
+	def show
+		@part = Part.find(params[:id])
+		@product = Product.find(params[:product_id])
+		@supplier = Supplier.find(params[:supplier_id])
+		
+		respond_to do |format|
+		format.html # show.html.erb
+		format.json { render json: @part }
+		end
+	end
 
-  # GET /parts/1
-  # GET /parts/1.json
-  def show
-    @part = Part.find(params[:id])
+	# GET /parts/new
+	# GET /parts/new.json
+	def new
+		@part = Part.new
+		@product = Product.find(params[:product_id])
+		@supplier = Supplier.find(params[:supplier_id])
+		
+		respond_to do |format|
+		format.html # new.html.erb
+		format.json { render json: @part }
+		end
+	end
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @part }
-    end
-  end
+	# GET /parts/1/edit
+	def edit
+		@part = Part.find(params[:id])
+		@product = Product.find(params[:product_id])
+		@supplier = Supplier.find(params[:supplier_id])
+	end
 
-  # GET /parts/new
-  # GET /parts/new.json
-  def new
-    @part = Part.new
+	# POST /parts
+	# POST /parts.json
+	def create
+		@part = Part.new(params[:part])
+		@product = Product.find(params[:product_id])
+		@supplier = Supplier.find(params[:supplier_id])
+		
+		respond_to do |format|
+		if @part.save
+			format.html { redirect_to @part, notice: 'Part was successfully created.' }
+			format.json { render json: @part, status: :created, location: @part }
+		else
+			format.html { render action: "new" }
+			format.json { render json: @part.errors, status: :unprocessable_entity }
+		end
+		end
+	end
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @part }
-    end
-  end
+	# PUT /parts/1
+	# PUT /parts/1.json
+	def update
+		@part = Part.find(params[:id])
+		@product = Product.find(params[:product_id])
+		@supplier = Supplier.find(params[:supplier_id])
+		
+		respond_to do |format|
+		if @part.update_attributes(params[:part])
+			format.html { redirect_to @part, notice: 'Part was successfully updated.' }
+			format.json { head :no_content }
+		else
+			format.html { render action: "edit" }
+			format.json { render json: @part.errors, status: :unprocessable_entity }
+		end
+		end
+	end
 
-  # GET /parts/1/edit
-  def edit
-    @part = Part.find(params[:id])
-  end
+	# DELETE /parts/1
+	# DELETE /parts/1.json
+	def destroy
+		@part = Part.find(params[:id])
+		@product = Product.find(params[:product_id])
+		@supplier = Supplier.find(params[:supplier_id])
+		@part.destroy
 
-  # POST /parts
-  # POST /parts.json
-  def create
-    @part = Part.new(params[:part])
-
-    respond_to do |format|
-      if @part.save
-        format.html { redirect_to @part, notice: 'Part was successfully created.' }
-        format.json { render json: @part, status: :created, location: @part }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @part.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /parts/1
-  # PUT /parts/1.json
-  def update
-    @part = Part.find(params[:id])
-
-    respond_to do |format|
-      if @part.update_attributes(params[:part])
-        format.html { redirect_to @part, notice: 'Part was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @part.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /parts/1
-  # DELETE /parts/1.json
-  def destroy
-    @part = Part.find(params[:id])
-    @part.destroy
-
-    respond_to do |format|
-      format.html { redirect_to parts_url }
-      format.json { head :no_content }
-    end
-  end
+		respond_to do |format|
+		format.html { redirect_to parts_url }
+		format.json { head :no_content }
+		end
+	end
 end
